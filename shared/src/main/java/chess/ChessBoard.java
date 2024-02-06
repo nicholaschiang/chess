nicholaschiang/chess

commit a33fa9f35ca4b40946189181a46953727782a140

feat(shared): implement remaining `ChessGame` methods
Ref: https://github.com/softwareconstruction240/softwareconstruction/blob/main/chess/1-chess-game/chess-game.md

shared/src/main/java/chess/ChessBoard.java
@@ -64,6 +64,20 @@ public class ChessBoard {
   }
 
   /**
+   * Creates a shallow clone of the given chessboard
+   *
+   * @param board the board to clone
+   */
+  public ChessBoard(ChessBoard board) {
+    this.board = new ChessPiece[8][8];
+    for (int row = 0; row < 8; row++) {
+      for (int col = 0; col < 8; col++) {
+        this.board[row][col] = board.board[row][col];
+      }
+    }
+  }
+
+  /**
    * Adds a chess piece to the chessboard
    *
    * @param position where to add the piece to
@@ -96,6 +110,23 @@ public class ChessBoard {
    */
   public ChessPiece getPiece(ChessPosition position) {
     return board[position.getRow() - 1][position.getColumn() - 1];
+  }
+
+  /**
+   * Finds the position of a chess piece on the chessboard
+   *
+   * @param piece the piece to find
+   * @return The position of the piece, or null if the piece is not on the board
+   */
+  public ChessPosition findPiece(ChessPiece piece) {
+    for (int row = 0; row < 8; row++) {
+      for (int col = 0; col < 8; col++) {
+        if (board[row][col].equals(piece)) {
+          return new ChessPosition(row + 1, col + 1);
+        }
+      }
+    }
+    return null;
   }
 
   /**

shared/src/main/java/chess/ChessGame.java
@@ -18,6 +18,17 @@ public class ChessGame {
   }
 
   /**
+   * Creates a shallow clone of the given chessboard
+   *
+   * @param board the board to clone
+   * @param teamTurn the team whose turn it is
+   */
+  public ChessGame(ChessBoard board, TeamColor teamTurn) {
+    this.board = new ChessBoard(board);
+    this.teamTurn = teamTurn;
+  }
+
+  /**
    * @return Which team's turn it is
    */
   public TeamColor getTeamTurn() {
@@ -56,51 +67,120 @@ public class ChessGame {
    * @throws InvalidMoveException if move is invalid
    */
   public void makeMove(ChessMove move) throws InvalidMoveException {
-    var piece = board.getPiece(move.getStartPosition());
-    if (piece.getTeamColor() != teamTurn) {
+    // A move is illegal if it’s not the corresponding team's turn.
+    if (board.getPiece(move.getStartPosition()).getTeamColor() != teamTurn)
       throw new InvalidMoveException("It is not your turn.");
-    }
 
-    if (validMoves(move.getStartPosition()).contains(move)) {
-      board.movePiece(move);
-      if (teamTurn == TeamColor.WHITE) {
-        teamTurn = TeamColor.BLACK;
-      } else {
-        teamTurn = TeamColor.WHITE;
-      }
-    } else {
+    // A move is illegal if the chess piece cannot move there.
+    if (!validMoves(move.getStartPosition()).contains(move))
       throw new InvalidMoveException("This piece cannot make this move.");
+
+    // A move is illegal if the move leaves the team's king in danger.
+    var tempGame = new ChessGame(board, teamTurn);
+    tempGame.getBoard().movePiece(move);
+    if (tempGame.isInCheck(teamTurn))
+      throw new InvalidMoveException("This move puts your king in check.");
+
+    // Otherwise, the move is legal, and the piece is moved.
+    board.movePiece(move);
+    if (teamTurn == TeamColor.WHITE) {
+      teamTurn = TeamColor.BLACK;
+    } else {
+      teamTurn = TeamColor.WHITE;
     }
   }
 
   /**
    * Determines if the given team is in check
    *
+   * <p>Returns true if the specified team’s King could be captured by an opposing piece.
+   *
    * @param teamColor which team to check for check
    * @return True if the specified team is in check
    */
   public boolean isInCheck(TeamColor teamColor) {
-    throw new RuntimeException("Not implemented");
+    // Find the position of the king.
+    ChessPiece king = new ChessPiece(teamColor, ChessPiece.PieceType.KING);
+    ChessPosition kingPosition = board.findPiece(king);
+
+    // For every enemy piece on the board, check if it can move to the same
+    // position as the king (i.e. capture the king).
+    for (int row = 1; row <= 8; row++) {
+      for (int col = 1; col <= 8; col++) {
+        ChessPosition position = new ChessPosition(row, col);
+        ChessPiece piece = board.getPiece(position);
+        if (piece != null && piece.getTeamColor() != teamColor) {
+          if (piece.pieceMoves(board, position).contains(new ChessMove(position, kingPosition))) {
+            return true;
+          }
+        }
+      }
+    }
+
+    // No enemy piece can capture the king.
+    return false;
   }
 
   /**
    * Determines if the given team is in checkmate
    *
+   * <p>Returns true if the given team has no way to protect their king from being captured.
+   *
    * @param teamColor which team to check for checkmate
    * @return True if the specified team is in checkmate
    */
   public boolean isInCheckmate(TeamColor teamColor) {
-    throw new RuntimeException("Not implemented");
+    // If we are not in check, we cannot be in checkmate.
+    if (!isInCheck(teamColor)) return false;
+
+    // For every friendly piece on the board, check if it can move to a position
+    // where we would no longer be in check.
+    for (int row = 1; row <= 8; row++) {
+      for (int col = 1; col <= 8; col++) {
+        ChessPosition position = new ChessPosition(row, col);
+        ChessPiece piece = board.getPiece(position);
+        if (piece != null && piece.getTeamColor() == teamColor) {
+          for (ChessMove move : validMoves(position)) {
+            ChessBoard tempBoard = new ChessBoard(board);
+            tempBoard.movePiece(move);
+            if (!isInCheck(teamColor)) {
+              return false;
+            }
+          }
+        }
+      }
+    }
+
+    // No friendly piece can move to a position where we would no longer be in
+    // check. Thus, we are in checkmate and have lost the game.
+    return true;
   }
 
   /**
    * Determines if the given team is in stalemate, which here is defined as having no valid moves
    *
+   * <p>Returns true if the given team has no legal moves and it is currently that team’s turn.
+   *
    * @param teamColor which team to check for stalemate
    * @return True if the specified team is in stalemate, otherwise false
    */
   public boolean isInStalemate(TeamColor teamColor) {
-    throw new RuntimeException("Not implemented");
+    // For every friendly piece on the board, check if it can move to a
+    // position. If any friendly piece can move, we are not in stalemate.
+    for (int row = 1; row <= 8; row++) {
+      for (int col = 1; col <= 8; col++) {
+        ChessPosition position = new ChessPosition(row, col);
+        ChessPiece piece = board.getPiece(position);
+        if (piece != null && piece.getTeamColor() == teamColor) {
+          if (!validMoves(position).isEmpty()) {
+            return false;
+          }
+        }
+      }
+    }
+
+    // No friendly piece can make a move. Thus, we are in stalemate.
+    return true;
   }
 
   /**

commit 152b1d878b225b18fd1d46d43223f097e244d040

fix(shared): address stack overflow issues
I was having some infinite recursion as multiple methods incorrectly
relied on `validMoves()` when they should just use `pieceMoves()`.

Fixes: a33fa9f35ca4 ("feat(shared): implement remaining `ChessGame` methods")

shared/src/main/java/chess/ChessBoard.java
@@ -121,7 +121,7 @@ public class ChessBoard {
   public ChessPosition findPiece(ChessPiece piece) {
     for (int row = 0; row < 8; row++) {
       for (int col = 0; col < 8; col++) {
-        if (board[row][col].equals(piece)) {
+        if (piece.equals(board[row][col])) {
           return new ChessPosition(row + 1, col + 1);
         }
       }
@@ -142,8 +142,6 @@ public class ChessBoard {
             && position.getRow() <= 8
             && position.getColumn() >= 1
             && position.getColumn() <= 8);
-    System.out.println(
-        String.format("Position %s is within board: %s", position, positionWithinBoard));
     return positionWithinBoard;
   }
 

shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.ArrayList;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -57,7 +58,19 @@ public class ChessGame {
    * @return Set of valid moves for requested piece, or null if no piece at startPosition
    */
   public Collection<ChessMove> validMoves(ChessPosition startPosition) {
-    return board.getPiece(startPosition).pieceMoves(board, startPosition);
+    // All the moves that the piece at startPosition can make.
+    var piece = board.getPiece(startPosition);
+    var allMoves = piece.pieceMoves(board, startPosition);
+
+    // All the moves that do not put the team's king in danger.
+    var validMoves = new ArrayList<ChessMove>();
+    for (var move : allMoves) {
+      var tempGame = new ChessGame(board, teamTurn);
+      tempGame.getBoard().movePiece(move);
+      if (!tempGame.isInCheck(piece.getTeamColor())) validMoves.add(move);
+    }
+
+    return validMoves;
   }
 
   /**
@@ -74,12 +87,6 @@ public class ChessGame {
     // A move is illegal if the chess piece cannot move there.
     if (!validMoves(move.getStartPosition()).contains(move))
       throw new InvalidMoveException("This piece cannot make this move.");
-
-    // A move is illegal if the move leaves the team's king in danger.
-    var tempGame = new ChessGame(board, teamTurn);
-    tempGame.getBoard().movePiece(move);
-    if (tempGame.isInCheck(teamTurn))
-      throw new InvalidMoveException("This move puts your king in check.");
 
     // Otherwise, the move is legal, and the piece is moved.
     board.movePiece(move);
@@ -103,6 +110,12 @@ public class ChessGame {
     ChessPiece king = new ChessPiece(teamColor, ChessPiece.PieceType.KING);
     ChessPosition kingPosition = board.findPiece(king);
 
+    // If there is no king, we cannot be in check.
+    if (kingPosition == null) {
+      System.out.println(String.format("No %s found in board:\n%s", king, board));
+      return false;
+    }
+
     // For every enemy piece on the board, check if it can move to the same
     // position as the king (i.e. capture the king).
     for (int row = 1; row <= 8; row++) {
@@ -110,8 +123,10 @@ public class ChessGame {
         ChessPosition position = new ChessPosition(row, col);
         ChessPiece piece = board.getPiece(position);
         if (piece != null && piece.getTeamColor() != teamColor) {
-          if (piece.pieceMoves(board, position).contains(new ChessMove(position, kingPosition))) {
-            return true;
+          for (ChessMove move : piece.pieceMoves(board, position)) {
+            if (move.getEndPosition().equals(kingPosition)) {
+              return true;
+            }
           }
         }
       }
@@ -140,10 +155,10 @@ public class ChessGame {
         ChessPosition position = new ChessPosition(row, col);
         ChessPiece piece = board.getPiece(position);
         if (piece != null && piece.getTeamColor() == teamColor) {
-          for (ChessMove move : validMoves(position)) {
-            ChessBoard tempBoard = new ChessBoard(board);
-            tempBoard.movePiece(move);
-            if (!isInCheck(teamColor)) {
+          for (ChessMove move : piece.pieceMoves(board, position)) {
+            ChessGame tempGame = new ChessGame(board, teamTurn);
+            tempGame.getBoard().movePiece(move);
+            if (!tempGame.isInCheck(teamColor)) {
               return false;
             }
           }

shared/src/main/java/chess/ChessPiece.java
@@ -228,9 +228,6 @@ public class ChessPiece {
    * @return Collection of valid moves
    */
   private Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
-    System.out.println(String.format("\nGenerating pawn moves for %s at: %s", this, myPosition));
-    System.out.println(String.format("Board:\n%s", board));
-
     // Pawns can only move forward one row (down for black, up for white)
     var moves = new ArrayList<ChessMove>();
     var oneForward =
@@ -248,10 +245,6 @@ public class ChessPiece {
               myPosition.getRow() + (pieceColor == ChessGame.TeamColor.WHITE ? 2 : -2),
               myPosition.getColumn());
       var twoForwardPiece = board.getPiece(twoForward);
-      System.out.println(
-          String.format(
-              "Pawn is at starting position, checking if there is a piece at %s: %s and %s: %s",
-              oneForward, oneForwardPiece, twoForward, twoForwardPiece));
       if (oneForwardPiece == null && twoForwardPiece == null)
         moves.add(new ChessMove(myPosition, twoForward));
     }
@@ -318,8 +311,6 @@ public class ChessPiece {
    */
   private Collection<ChessMove> generateMoves(
       ChessPosition[] directions, ChessBoard board, ChessPosition myPosition) {
-    System.out.println(String.format("\nGenerating moves for %s at: %s", this, myPosition));
-    System.out.println(String.format("Board:\n%s", board));
 
     var moves = new ArrayList<ChessMove>();
     for (var direction : directions) {
@@ -329,16 +320,11 @@ public class ChessPiece {
               myPosition.getColumn() + direction.getColumn());
       while (ChessBoard.isPositionWithinBoard(nextPosition)) {
         var piece = board.getPiece(nextPosition);
-        System.out.println(String.format("Piece at %s: %s", nextPosition, piece));
         if (piece == null) {
           var chessMove = new ChessMove(myPosition, nextPosition);
-          System.out.println(String.format("There is no piece at: %s", nextPosition));
-          System.out.println(String.format("Adding move: %s", chessMove));
           // If there is no piece at the next position, we can move there
           moves.add(chessMove);
           if (isPieceContinuous()) {
-            System.out.println(
-                String.format("Piece is continuous, continuing in direction: %s", direction));
             // Keep moving in the same direction until we hit a piece
             nextPosition =
                 new ChessPosition(