nicholaschiang/chess

commit 20c0b999b7412a1e9752b0b053fbd8c73b293b02

feat(shared/ChessPiece): add pawn promotion moves

shared/src/main/java/chess/ChessPiece.java
@@ -225,17 +225,28 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     private Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
+        System.out.println(String.format("\nGenerating pawn moves for %s at: %s", this, myPosition));
+        System.out.println(String.format("Board:\n%s", board));
+
         // Pawns can only move forward one row (down for black, up for white)
-        ChessPosition[] directions = { new ChessPosition(pieceColor == ChessGame.TeamColor.WHITE ? 1 : -1, 0) };
-        var moves = generateMoves(directions, board, myPosition);
+        var moves = new ArrayList<ChessMove>();
+        var oneForward = new ChessPosition(
+            myPosition.getRow() + (pieceColor == ChessGame.TeamColor.WHITE ? 1 : -1), 
+            myPosition.getColumn()
+        );
+        var oneForwardPiece = board.getPiece(oneForward);
+        if (ChessBoard.isPositionWithinBoard(oneForward) && oneForwardPiece == null)
+            moves.add(new ChessMove(myPosition, oneForward));
 
         // If it is the pawn's first move, it can move two squares forward
-        if (myPosition.getRow() == (pieceColor == ChessGame.TeamColor.WHITE ? 1 : 6)) {
+        if (myPosition.getRow() == (pieceColor == ChessGame.TeamColor.WHITE ? 2 : 7)) {
             var twoForward = new ChessPosition(
                 myPosition.getRow() + (pieceColor == ChessGame.TeamColor.WHITE ? 2 : -2),
                 myPosition.getColumn()
             );
-            if (board.getPiece(twoForward) == null)
+            var twoForwardPiece = board.getPiece(twoForward);
+            System.out.println(String.format("Pawn is at starting position, checking if there is a piece at %s: %s and %s: %s", oneForward, oneForwardPiece, twoForward, twoForwardPiece));
+            if (oneForwardPiece == null && twoForwardPiece == null)
                 moves.add(new ChessMove(myPosition, twoForward));
         }
 
@@ -259,8 +270,20 @@ public class ChessPiece {
                 moves.add(new ChessMove(myPosition, rightDiagonal));
         }
 
-        // Perhaps I have to add moves for every possible promotion if one of 
-        // the moves included in `moves` is a promotion?
+        // Add moves for every possible promotion if one of the moves included in `moves` is a promotion
+        var promotionMoves = new ArrayList<ChessMove>();
+        var duplicateMoves = new ArrayList<ChessMove>();
+        for (var move : moves) {
+            if (move.getEndPosition().getRow() == (pieceColor == ChessGame.TeamColor.WHITE ? 8 : 1)) {
+                promotionMoves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.QUEEN));
+                promotionMoves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.ROOK));
+                promotionMoves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.BISHOP));
+                promotionMoves.add(new ChessMove(move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.KNIGHT));
+                duplicateMoves.add(move);
+            }
+        }
+        moves.removeAll(duplicateMoves);
+        moves.addAll(promotionMoves);
 
         return moves;
     }
@@ -282,6 +305,7 @@ public class ChessPiece {
     private Collection<ChessMove> generateMoves(ChessPosition[] directions, ChessBoard board, ChessPosition myPosition) {
         System.out.println(String.format("\nGenerating moves for %s at: %s", this, myPosition));
         System.out.println(String.format("Board:\n%s", board));
+        
         var moves = new ArrayList<ChessMove>();
         for (var direction : directions) {
             var nextPosition = new ChessPosition(

shared/src/main/java/chess/ChessMove.java
@@ -38,7 +38,11 @@ public class ChessMove {
 
     @Override
     public String toString() {
-        return String.format("%s -> %s", startPosition, endPosition);
+        var move = String.format("%s -> %s", startPosition, endPosition);
+        if (promotionPiece != null) {
+            move += String.format(" (%s)", promotionPiece);
+        }
+        return move;
     }
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition) {

commit c8883ef3aa15c5b4c64fcbcaaba101ec7896cc03

fix(shared): use `HashSet` instead of `ArrayList`
This patch fixes an issue where using `ArrayList` caused some test
failures locally. It seems like running in the autograder environment,
the tests passed. I suspect the test suite may be different or the
autograder environment may polyfill `ArrayList` with `HashSet`.

Fixes: 20c0b999b741 ("feat(shared/ChessPiece): add pawn promotion moves")

shared/src/main/java/chess/ChessGame.java
@@ -1,7 +1,7 @@
 package chess;
 
 import java.util.Collection;
-import java.util.ArrayList;
+import java.util.HashSet;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -63,7 +63,7 @@ public class ChessGame {
     var allMoves = piece.pieceMoves(board, startPosition);
 
     // All the moves that do not put the team's king in danger.
-    var validMoves = new ArrayList<ChessMove>();
+    var validMoves = new HashSet<ChessMove>();
     for (var move : allMoves) {
       var tempGame = new ChessGame(board, teamTurn);
       tempGame.getBoard().movePiece(move);

shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,6 @@
 package chess;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Collection;
 import java.util.Map;
 
@@ -118,7 +118,7 @@ public class ChessPiece {
       case PAWN:
         return pawnMoves(board, myPosition);
       default:
-        return new ArrayList<ChessMove>();
+        return new HashSet<ChessMove>();
     }
   }
 
@@ -229,7 +229,7 @@ public class ChessPiece {
    */
   private Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
     // Pawns can only move forward one row (down for black, up for white)
-    var moves = new ArrayList<ChessMove>();
+    var moves = new HashSet<ChessMove>();
     var oneForward =
         new ChessPosition(
             myPosition.getRow() + (pieceColor == ChessGame.TeamColor.WHITE ? 1 : -1),
@@ -270,22 +270,22 @@ public class ChessPiece {
     }
 
     // Add moves for every possible promotion if one of the moves included in `moves` is a promotion
-    var promotionMoves = new ArrayList<ChessMove>();
-    var duplicateMoves = new ArrayList<ChessMove>();
+    var promotionMoves = new HashSet<ChessMove>();
+    var duplicateMoves = new HashSet<ChessMove>();
     for (var move : moves) {
       if (move.getEndPosition().getRow() == (pieceColor == ChessGame.TeamColor.WHITE ? 8 : 1)) {
-        promotionMoves.add(
-            new ChessMove(
-                move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.QUEEN));
-        promotionMoves.add(
-            new ChessMove(
-                move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.ROOK));
         promotionMoves.add(
             new ChessMove(
                 move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.BISHOP));
         promotionMoves.add(
             new ChessMove(
                 move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.KNIGHT));
+        promotionMoves.add(
+            new ChessMove(
+                move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.ROOK));
+        promotionMoves.add(
+            new ChessMove(
+                move.getStartPosition(), move.getEndPosition(), ChessPiece.PieceType.QUEEN));
         duplicateMoves.add(move);
       }
     }
@@ -312,7 +312,7 @@ public class ChessPiece {
   private Collection<ChessMove> generateMoves(
       ChessPosition[] directions, ChessBoard board, ChessPosition myPosition) {
 
-    var moves = new ArrayList<ChessMove>();
+    var moves = new HashSet<ChessMove>();
     for (var direction : directions) {
       var nextPosition =
           new ChessPosition(