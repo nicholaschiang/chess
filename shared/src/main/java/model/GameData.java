1a2f5358d6f656088dbef5d5f36c18cfd38da4a4
Nicholas Chiang <nicholas.h.chiang@gmail.com>
2024-02-28 16:38:00 -0700

    feat(shared/model): define basic data model classes
    Ref: https://github.com/softwareconstruction240/softwareconstruction/blob/main/chess/3-web-api/web-api.md#data-model-classes

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,40 @@
+package model;
+
+import chess.ChessGame;
+
+public class GameData {
+  private int gameId;
+  private String whiteUsername;
+  private String blackUsername;
+  private String gameName;
+  private ChessGame game;
+
+  public GameData(
+      int gameId, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    this.gameId = gameId;
+    this.whiteUsername = whiteUsername;
+    this.blackUsername = blackUsername;
+    this.gameName = gameName;
+    this.game = game;
+  }
+
+  public int getGameId() {
+    return gameId;
+  }
+
+  public String getWhiteUsername() {
+    return whiteUsername;
+  }
+
+  public String getBlackUsername() {
+    return blackUsername;
+  }
+
+  public String getGameName() {
+    return gameName;
+  }
+
+  public ChessGame getGame() {
+    return game;
+  }
+}
