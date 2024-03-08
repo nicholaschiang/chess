nicholaschiang/chess

commit a687147ccf5bc19454e5bde078ed8ca26e1f9290

feat(server/dataAccess): implement SQL DAOs
This patch implements some very scrappy MySQL data access objects. My
implementation details were heavily inspired by the `petshop` example.

Ref: https://github.com/softwareconstruction240/softwareconstruction/blob/main/petshop/server/src/main/dataaccess/MySqlDataAccess.java

server/src/main/java/dataAccess/SQLAuthDataAccess.java
@@ -1,32 +1,58 @@
 package dataAccess;
 
-import java.util.HashMap;
-import java.util.Map;
+import exception.ResponseException;
 import model.AuthData;
 
-public class SQLAuthDataAccess implements AuthDataAccess {
+public class SQLAuthDataAccess extends SQLDataAccess implements AuthDataAccess {
+  protected final String[] createStatements = {
+    """
+    CREATE TABLE IF NOT EXISTS auth (
+      `username` varchar(256) NOT NULL,
+      `authToken` varchar(256) NOT NULL,
+      PRIMARY KEY (`authToken`),
+      INDEX(username),
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+    """
+  };
 
-  public SQLAuthDataAccess() {}
-
-  private Map<String, AuthData> auths = new HashMap<String, AuthData>();
+  public SQLAuthDataAccess() throws ResponseException {
+    super();
+  }
 
   // Clear all auths.
-  public void clear() {
-    auths.clear();
+  public void clear() throws ResponseException {
+    var statement = "TRUNCATE auth";
+    executeUpdate(statement);
   }
 
   // Create a new authorization.
-  public void createAuth(AuthData auth) {
-    auths.put(auth.getAuthToken(), auth);
+  public AuthData createAuth(AuthData auth) throws ResponseException {
+    var statement = "INSERT INTO auth (username, authToken) VALUES (?, ?)";
+    executeUpdate(statement, auth.getUsername(), auth.getAuthToken());
+    return auth;
   }
 
   // Retrieve an authorization given an authToken.
-  public AuthData getAuth(String authToken) {
-    return auths.get(authToken);
+  public AuthData getAuth(String authToken) throws ResponseException {
+    try (var conn = DatabaseManager.getConnection()) {
+      var statement = "SELECT username, authToken FROM auth WHERE authToken = ?";
+      try (var ps = conn.prepareStatement(statement)) {
+        ps.setString(1, authToken);
+        try (var rs = ps.executeQuery()) {
+          if (rs.next()) {
+            return new AuthData(rs.getString("username"), rs.getString("authToken"));
+          }
+        }
+      }
+    } catch (Exception e) {
+      throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+    }
+    return null;
   }
 
   // Delete an authorization so that it is no longer valid.
-  public void deleteAuth(String authToken) {
-    auths.remove(authToken);
+  public void deleteAuth(String authToken) throws ResponseException {
+    var statement = "DELETE FROM auth WHERE authToken = ?";
+    executeUpdate(statement, authToken);
   }
 }

server/src/main/java/dataAccess/SQLDataAccess.java
@@ -1,0 +1,61 @@
+package dataAccess;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+import exception.ResponseException;
+import java.sql.*;
+
+abstract class SQLDataAccess {
+  public SQLDataAccess() throws ResponseException {
+    configureDatabase();
+  }
+
+  protected final String[] createStatements = {};
+
+  private void configureDatabase() throws ResponseException {
+    try {
+      DatabaseManager.createDatabase();
+      try (var conn = DatabaseManager.getConnection()) {
+        for (var statement : createStatements) {
+          try (var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+          }
+        }
+      }
+    } catch (SQLException ex) {
+      throw new ResponseException(
+          500, String.format("Unable to configure database: %s", ex.getMessage()));
+    } catch (DataAccessException ex) {
+      throw new ResponseException(
+          500, String.format("Unable to configure database: %s", ex.getMessage()));
+    }
+  }
+
+  protected int executeUpdate(String statement, Object... params) throws ResponseException {
+    try (var conn = DatabaseManager.getConnection()) {
+      try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+        for (var i = 0; i < params.length; i++) {
+          var param = params[i];
+          if (param instanceof String p) ps.setString(i + 1, p);
+          else if (param instanceof Integer p) ps.setInt(i + 1, p);
+          else if (param == null) ps.setNull(i + 1, NULL);
+        }
+        ps.executeUpdate();
+
+        var rs = ps.getGeneratedKeys();
+        if (rs.next()) {
+          return rs.getInt(1);
+        }
+
+        return 0;
+      }
+    } catch (SQLException e) {
+      throw new ResponseException(
+          500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+    } catch (DataAccessException e) {
+      throw new ResponseException(
+          500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+    }
+  }
+}

server/src/main/java/dataAccess/SQLGameDataAccess.java
@@ -1,49 +1,111 @@
 package dataAccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
+import exception.ResponseException;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import model.GameData;
 
-public class SQLGameDataAccess implements GameDataAccess {
-  private ArrayList<GameData> games = new ArrayList<GameData>();
+public class SQLGameDataAccess extends SQLDataAccess implements GameDataAccess {
+  protected final String[] createStatements = {
+    """
+    CREATE TABLE IF NOT EXISTS game (
+      `gameID` int NOT NULL AUTO_INCREMENT,
+      `whiteUsername` varchar(256) NOT NULL,
+      `blackUsername` varchar(256) NOT NULL,
+      `gameName` varchar(256) NOT NULL,
+      `json` TEXT DEFAULT NULL,
+      PRIMARY KEY (`gameID`),
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+    """
+  };
 
-  // Get the game index from the game ID.
-  private static int getGameIndexFromID(int gameID) {
-    return gameID - 1;
+  public SQLGameDataAccess() throws ResponseException {
+    super();
   }
 
   // Clears all games.
-  public void clear() {
-    games.clear();
+  public void clear() throws ResponseException {
+    var statement = "TRUNCATE game";
+    executeUpdate(statement);
   }
 
   // Create a new game.
-  public void createGame(GameData game) {
-    games.add(game);
-    game.setGameId(games.size());
-  }
-
-  // Retrieve a specified game with the given game ID.
-  public GameData getGame(int gameID) {
-    var index = getGameIndexFromID(gameID);
-    if (index < 0 || index > games.size() - 1) {
-      return null;
-    }
-    return games.get(getGameIndexFromID(gameID));
-  }
-
-  // Retrieve all games.
-  public GameData[] listGames() {
-    return games.toArray(new GameData[0]);
+  public GameData createGame(GameData game) throws ResponseException {
+    var statement =
+        "INSERT INTO game (whiteUsername, blackUsername, gameName, json) VALUES (?, ?, ?, ?)";
+    var json = new Gson().toJson(game.getGame());
+    var id =
+        executeUpdate(
+            statement, game.getWhiteUsername(), game.getBlackUsername(), game.getGameName(), json);
+    return new GameData(
+        id, game.getWhiteUsername(), game.getBlackUsername(), game.getGameName(), game.getGame());
   }
 
   // Updates a chess game. It should replace the chess game string corresponding
   // to a given gameID. This is used when players join a game or when a move is
   // made.
-  public void updateGame(int gameID, GameData game) throws DataAccessException {
-    var index = getGameIndexFromID(gameID);
-    if (index < 0 || index > games.size() - 1) {
-      throw new DataAccessException("game not found");
+  public GameData updateGame(int gameID, GameData game) throws ResponseException {
+    var statement =
+        "UPDATE game SET whiteUsername = ?, blackUsername = ?, gameName = ?, json = ? WHERE gameID"
+            + " = ?";
+    var id =
+        executeUpdate(
+            statement,
+            game.getWhiteUsername(),
+            game.getBlackUsername(),
+            game.getGameName(),
+            new Gson().toJson(game.getGame()),
+            gameID);
+    return new GameData(
+        id, game.getWhiteUsername(), game.getBlackUsername(), game.getGameName(), game.getGame());
+  }
+
+  // Retrieve a specified game with the given game ID.
+  public GameData getGame(int gameID) throws ResponseException {
+    try (var conn = DatabaseManager.getConnection()) {
+      var statement = "SELECT * FROM game WHERE gameID = ?";
+      try (var ps = conn.prepareStatement(statement)) {
+        ps.setInt(1, gameID);
+        try (var rs = ps.executeQuery()) {
+          if (rs.next()) {
+            return readGame(rs);
+          }
+        }
+      }
+    } catch (Exception e) {
+      throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
     }
-    games.set(index, game);
+    return null;
+  }
+
+  // Retrieve all games.
+  public Collection<GameData> listGames() throws ResponseException {
+    var result = new ArrayList<GameData>();
+    try (var conn = DatabaseManager.getConnection()) {
+      var statement = "SELECT * FROM game";
+      try (var ps = conn.prepareStatement(statement)) {
+        try (var rs = ps.executeQuery()) {
+          while (rs.next()) {
+            result.add(readGame(rs));
+          }
+        }
+      }
+    } catch (Exception e) {
+      throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+    }
+    return result;
+  }
+
+  private GameData readGame(ResultSet rs) throws SQLException {
+    var id = rs.getInt("id");
+    var whiteUsername = rs.getString("whiteUsername");
+    var blackUsername = rs.getString("blackUsername");
+    var gameName = rs.getString("gameName");
+    var json = rs.getString("json");
+    var game = new Gson().fromJson(json, ChessGame.class);
+    return new GameData(id, whiteUsername, blackUsername, gameName, game);
   }
 }

server/src/main/java/dataAccess/SQLUserDataAccess.java
@@ -1,24 +1,57 @@
 package dataAccess;
 
-import java.util.HashMap;
-import java.util.Map;
+import exception.ResponseException;
+import java.sql.*;
 import model.UserData;
 
-public class SQLUserDataAccess implements UserDataAccess {
-  private Map<String, UserData> users = new HashMap<String, UserData>();
+public class SQLUserDataAccess extends SQLDataAccess implements UserDataAccess {
+  protected final String[] createStatements = {
+    """
+    CREATE TABLE IF NOT EXISTS user (
+      `username` varchar(256) NOT NULL,
+      `password` varchar(256) NOT NULL,
+      `email` varchar(256) NOT NULL,
+      PRIMARY KEY (`username`),
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+    """
+  };
+
+  public SQLUserDataAccess() throws ResponseException {
+    super();
+  }
 
   // Clears all users.
-  public void clear() {
-    users.clear();
+  public void clear() throws ResponseException {
+    var statement = "TRUNCATE user";
+    executeUpdate(statement);
   }
 
   // Create a new user.
-  public void createUser(UserData user) {
-    users.put(user.getUsername(), user);
+  public UserData createUser(UserData user) throws ResponseException {
+    var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
+    executeUpdate(statement, user.getUsername(), user.getPassword(), user.getEmail());
+    return new UserData(user.getUsername(), user.getPassword(), user.getEmail());
   }
 
   // Retrieve a user with the given username.
-  public UserData getUser(String username) {
-    return users.get(username);
+  public UserData getUser(String username) throws ResponseException {
+    try (var conn = DatabaseManager.getConnection()) {
+      var statement = "SELECT username, password, email FROM user WHERE username=?";
+      try (var ps = conn.prepareStatement(statement)) {
+        ps.setString(1, username);
+        try (var rs = ps.executeQuery()) {
+          if (rs.next()) {
+            return readUser(rs);
+          }
+        }
+      }
+    } catch (Exception e) {
+      throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+    }
+    return null;
+  }
+
+  private UserData readUser(ResultSet rs) throws SQLException {
+    return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
   }
 }

server/src/main/java/server/Server.java
@@ -9,12 +9,21 @@ import spark.*;
 
 public class Server {
   private Gson gson = new Gson();
-  private UserDataAccess userDataAccess = new MemoryUserDataAccess();
-  private AuthDataAccess authDataAccess = new MemoryAuthDataAccess();
-  private GameDataAccess gameDataAccess = new MemoryGameDataAccess();
-  private UserService userService = new UserService(userDataAccess, authDataAccess);
-  private GameService gameService = new GameService(authDataAccess, gameDataAccess);
-  private DataService dataService = new DataService(userDataAccess, authDataAccess, gameDataAccess);
+  private UserDataAccess userDataAccess;
+  private AuthDataAccess authDataAccess;
+  private GameDataAccess gameDataAccess;
+  private UserService userService;
+  private GameService gameService;
+  private DataService dataService;
+
+  public Server() throws ResponseException {
+    userDataAccess = new SQLUserDataAccess();
+    authDataAccess = new SQLAuthDataAccess();
+    gameDataAccess = new SQLGameDataAccess();
+    userService = new UserService(userDataAccess, authDataAccess);
+    gameService = new GameService(authDataAccess, gameDataAccess);
+    dataService = new DataService(userDataAccess, authDataAccess, gameDataAccess);
+  }
 
   public int run(int desiredPort) {
     Spark.port(desiredPort);
@@ -32,6 +41,9 @@ public class Server {
           try {
             dataService.clearData();
             return "";
+          } catch (ResponseException e) {
+            response.status(e.getStatusCode());
+            return gson.toJson(new ErrorResponse(e.getMessage()));
           } catch (Exception e) {
             response.status(500);
             return gson.toJson(new ErrorResponse(e.getMessage()));

server/src/main/java/service/DataService.java
@@ -1,5 +1,6 @@
 package service;
 
+import exception.ResponseException;
 import dataAccess.*;
 
 public class DataService {
@@ -14,7 +15,7 @@ public class DataService {
     this.gameDataAccess = gameDataAccess;
   }
 
-  public void clearData() {
+  public void clearData() throws ResponseException {
     userDataAccess.clear();
     authDataAccess.clear();
     gameDataAccess.clear();

server/src/main/java/dataAccess/AuthDataAccess.java
@@ -1,14 +1,15 @@
 package dataAccess;
 
+import exception.ResponseException;
 import model.AuthData;
 
 public interface AuthDataAccess extends DataAccess {
   // Create a new authorization.
-  public void createAuth(AuthData auth);
+  public AuthData createAuth(AuthData auth) throws ResponseException;
 
   // Retrieve an authorization given an authToken.
-  public AuthData getAuth(String authToken);
+  public AuthData getAuth(String authToken) throws ResponseException;
 
   // Delete an authorization so that it is no longer valid.
-  public void deleteAuth(String authToken);
+  public void deleteAuth(String authToken) throws ResponseException;
 }

server/src/main/java/dataAccess/DataAccess.java
@@ -1,6 +1,8 @@
 package dataAccess;
 
+import exception.ResponseException;
+
 interface DataAccess {
   // A method for clearing all data from the database. Used during testing.
-  public void clear();
+  public void clear() throws ResponseException;
 }

commit 1b66942fa3713fd67e97c015c63f04a9dce3fb51

fix(server): use `DELETE FROM` instead of `TRUNCATE`
Because I added foreign key constraints, I cannot use `TRUNCATE` without
possibly compromising those foreign keys. Instead, I use a `DELETE FROM`
which (given the scope of this project) should be performant enough.

I also ensure to `DELETE FROM` the table depending on the `user` foreign
keys before I try to `DELETE FROM` user.

Fixes: a687147ccf5b ("feat(server/dataAccess): implement SQL DAOs")

Makefile
@@ -12,5 +12,8 @@ run: compile
 format:
 	find . -name "*.java" -type f -print | xargs google-java-format --replace
 
+test:
+	mvn test -Dtest="StandardAPITests#loginInvalidUser" -Dsurefire.failIfNoSpecifiedTests=false
+
 db:
 	docker run --name chess -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=chess -d mysql

server/src/main/java/ServerMain.java
@@ -4,13 +4,8 @@ public class ServerMain {
   private static Server server;
 
   public static void main(String[] args) {
-    try {
-      server = new Server();
-      var port = server.run(0);
-      System.out.println("Started server! Visit: http://localhost:" + port);
-    } catch (Throwable ex) {
-      System.out.println("Failed to start server: " + ex.getMessage());
-      ex.printStackTrace();
-    }
+    server = new Server();
+    var port = server.run(0);
+    System.out.println("Started server! Visit: http://localhost:" + port);
   }
 }

server/src/main/java/dataAccess/SQLAuthDataAccess.java
@@ -21,7 +21,7 @@ public class SQLAuthDataAccess extends SQLDataAccess implements AuthDataAccess {
 
   // Clear all auths.
   public void clear() throws ResponseException {
-    var statement = "TRUNCATE auth";
+    var statement = "DELETE FROM auth";
     executeUpdate(statement);
   }
 

server/src/main/java/dataAccess/SQLDataAccess.java
@@ -42,6 +42,7 @@ abstract class SQLDataAccess {
           else if (param instanceof Integer p) ps.setInt(i + 1, p);
           else if (param == null) ps.setNull(i + 1, NULL);
         }
+        System.out.println("Executing: " + ps.toString());
         ps.executeUpdate();
 
         var rs = ps.getGeneratedKeys();

server/src/main/java/dataAccess/SQLGameDataAccess.java
@@ -20,7 +20,7 @@ public class SQLGameDataAccess extends SQLDataAccess implements GameDataAccess {
       PRIMARY KEY (gameID),
       FOREIGN KEY (whiteUsername) REFERENCES user(username),
       FOREIGN KEY (blackUsername) REFERENCES user(username)
-    )
+    );
     """
   };
 
@@ -30,7 +30,7 @@ public class SQLGameDataAccess extends SQLDataAccess implements GameDataAccess {
 
   // Clears all games.
   public void clear() throws ResponseException {
-    var statement = "TRUNCATE game";
+    var statement = "DELETE FROM game";
     executeUpdate(statement);
   }
 

server/src/main/java/dataAccess/SQLUserDataAccess.java
@@ -22,12 +22,13 @@ public class SQLUserDataAccess extends SQLDataAccess implements UserDataAccess {
 
   // Clears all users.
   public void clear() throws ResponseException {
-    var statement = "TRUNCATE user";
+    var statement = "DELETE FROM user";
     executeUpdate(statement);
   }
 
   // Create a new user.
   public UserData createUser(UserData user) throws ResponseException {
+    System.out.println("Creating user: " + user.getUsername());
     var statement = "INSERT INTO user (username, password, email) VALUES (?, ?, ?)";
     executeUpdate(statement, user.getUsername(), user.getPassword(), user.getEmail());
     return new UserData(user.getUsername(), user.getPassword(), user.getEmail());
@@ -35,10 +36,12 @@ public class SQLUserDataAccess extends SQLDataAccess implements UserDataAccess {
 
   // Retrieve a user with the given username.
   public UserData getUser(String username) throws ResponseException {
+    System.out.println("Getting user: " + username);
     try (var conn = DatabaseManager.getConnection()) {
       var statement = "SELECT username, password, email FROM user WHERE username=?";
       try (var ps = conn.prepareStatement(statement)) {
         ps.setString(1, username);
+        System.out.println("Executing: " + ps.toString());
         try (var rs = ps.executeQuery()) {
           if (rs.next()) {
             return readUser(rs);
@@ -52,6 +55,10 @@ public class SQLUserDataAccess extends SQLDataAccess implements UserDataAccess {
   }
 
   private UserData readUser(ResultSet rs) throws SQLException {
-    return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+    var username = rs.getString("username");
+    var password = rs.getString("password");
+    var email = rs.getString("email");
+    System.out.println("Read user: " + username + " " + password + " " + email);
+    return new UserData(username, password, email);
   }
 }

server/src/main/java/server/Server.java
@@ -16,13 +16,18 @@ public class Server {
   private GameService gameService;
   private DataService dataService;
 
-  public Server() throws ResponseException {
-    userDataAccess = new SQLUserDataAccess();
-    authDataAccess = new SQLAuthDataAccess();
-    gameDataAccess = new SQLGameDataAccess();
-    userService = new UserService(userDataAccess, authDataAccess);
-    gameService = new GameService(authDataAccess, gameDataAccess);
-    dataService = new DataService(userDataAccess, authDataAccess, gameDataAccess);
+  public Server() {
+    try {
+      userDataAccess = new SQLUserDataAccess();
+      authDataAccess = new SQLAuthDataAccess();
+      gameDataAccess = new SQLGameDataAccess();
+      userService = new UserService(userDataAccess, authDataAccess);
+      gameService = new GameService(authDataAccess, gameDataAccess);
+      dataService = new DataService(userDataAccess, authDataAccess, gameDataAccess);
+    } catch (Throwable ex) {
+      System.out.println("Failed to initialize server: " + ex.getMessage());
+      ex.printStackTrace();
+    }
   }
 
   public int run(int desiredPort) {

server/src/main/java/service/DataService.java
@@ -16,8 +16,9 @@ public class DataService {
   }
 
   public void clearData() throws ResponseException {
-    userDataAccess.clear();
+    System.out.println("Clearing data...");
     authDataAccess.clear();
     gameDataAccess.clear();
+    userDataAccess.clear();
   }
 }

server/src/main/java/service/UserService.java
@@ -15,11 +15,13 @@ public class UserService {
   }
 
   public AuthData registerUser(UserData user) throws ResponseException {
+    System.out.println("Registering user: "+ user.getUsername());
     if (user.getUsername() == null || user.getPassword() == null) {
       throw new ResponseException(400, "bad request");
     }
     UserData existingUser = userDataAccess.getUser(user.getUsername());
     if (existingUser != null) {
+      System.out.println("User already exists: " + user.getUsername());
       throw new ResponseException(403, "already taken");
     }
     userDataAccess.createUser(user);
@@ -30,6 +32,7 @@ public class UserService {
   }
 
   public AuthData loginUser(LoginRequest loginRequest) throws ResponseException {
+    System.out.println("Logging in user: "+ loginRequest.getUsername());
     UserData user = userDataAccess.getUser(loginRequest.getUsername());
     if (user == null || !user.getPassword().equals(loginRequest.getPassword())) {
       throw new ResponseException(401, "unauthorized");
@@ -41,6 +44,7 @@ public class UserService {
   }
 
   public void logoutUser(String authToken) throws ResponseException {
+    System.out.println("Logging out user: "+ authToken);
     if (authDataAccess.getAuth(authToken) == null) {
       throw new ResponseException(401, "unauthorized");
     }