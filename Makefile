nicholaschiang/chess

commit a687147ccf5bc19454e5bde078ed8ca26e1f9290

feat(server/dataAccess): implement SQL DAOs
This patch implements some very scrappy MySQL data access objects. My
implementation details were heavily inspired by the `petshop` example.

Ref: https://github.com/softwareconstruction240/softwareconstruction/blob/main/petshop/server/src/main/dataaccess/MySqlDataAccess.java

server/src/main/java/dataAccess/SQLDataAccess.java
@@ -1,0 +1,61 @@
+package dataAccess;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+import exception.ResponseException;
+import java.sql.*;
+
+abstract class SQLDataAccess {
+  public SQLDataAccess() throws ResponseException {
+    configureDatabase();
+  }
+
+  protected final String[] createStatements = {};
+
+  private void configureDatabase() throws ResponseException {
+    try {
+      DatabaseManager.createDatabase();
+      try (var conn = DatabaseManager.getConnection()) {
+        for (var statement : createStatements) {
+          try (var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+          }
+        }
+      }
+    } catch (SQLException ex) {
+      throw new ResponseException(
+          500, String.format("Unable to configure database: %s", ex.getMessage()));
+    } catch (DataAccessException ex) {
+      throw new ResponseException(
+          500, String.format("Unable to configure database: %s", ex.getMessage()));
+    }
+  }
+
+  protected int executeUpdate(String statement, Object... params) throws ResponseException {
+    try (var conn = DatabaseManager.getConnection()) {
+      try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+        for (var i = 0; i < params.length; i++) {
+          var param = params[i];
+          if (param instanceof String p) ps.setString(i + 1, p);
+          else if (param instanceof Integer p) ps.setInt(i + 1, p);
+          else if (param == null) ps.setNull(i + 1, NULL);
+        }
+        ps.executeUpdate();
+
+        var rs = ps.getGeneratedKeys();
+        if (rs.next()) {
+          return rs.getInt(1);
+        }
+
+        return 0;
+      }
+    } catch (SQLException e) {
+      throw new ResponseException(
+          500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+    } catch (DataAccessException e) {
+      throw new ResponseException(
+          500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+    }
+  }
+}

server/src/main/java/dataAccess/SQLGameDataAccess.java
@@ -1,49 +1,111 @@
 package dataAccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
+import exception.ResponseException;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import model.GameData;
 
-public class SQLGameDataAccess implements GameDataAccess {
-  private ArrayList<GameData> games = new ArrayList<GameData>();
+public class SQLGameDataAccess extends SQLDataAccess implements GameDataAccess {
+  protected final String[] createStatements = {
+    """
+    CREATE TABLE IF NOT EXISTS game (
+      `gameID` int NOT NULL AUTO_INCREMENT,
+      `whiteUsername` varchar(256) NOT NULL,
+      `blackUsername` varchar(256) NOT NULL,
+      `gameName` varchar(256) NOT NULL,
+      `json` TEXT DEFAULT NULL,
+      PRIMARY KEY (`gameID`),
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+    """
+  };
 
-  // Get the game index from the game ID.
-  private static int getGameIndexFromID(int gameID) {
-    return gameID - 1;
+  public SQLGameDataAccess() throws ResponseException {
+    super();
   }
 
   // Clears all games.
-  public void clear() {
-    games.clear();
+  public void clear() throws ResponseException {
+    var statement = "TRUNCATE game";
+    executeUpdate(statement);
   }
 
   // Create a new game.
-  public void createGame(GameData game) {
-    games.add(game);
-    game.setGameId(games.size());
-  }
-
-  // Retrieve a specified game with the given game ID.
-  public GameData getGame(int gameID) {
-    var index = getGameIndexFromID(gameID);
-    if (index < 0 || index > games.size() - 1) {
-      return null;
-    }
-    return games.get(getGameIndexFromID(gameID));
-  }
-
-  // Retrieve all games.
-  public GameData[] listGames() {
-    return games.toArray(new GameData[0]);
+  public GameData createGame(GameData game) throws ResponseException {
+    var statement =
+        "INSERT INTO game (whiteUsername, blackUsername, gameName, json) VALUES (?, ?, ?, ?)";
+    var json = new Gson().toJson(game.getGame());
+    var id =
+        executeUpdate(
+            statement, game.getWhiteUsername(), game.getBlackUsername(), game.getGameName(), json);
+    return new GameData(
+        id, game.getWhiteUsername(), game.getBlackUsername(), game.getGameName(), game.getGame());
   }
 
   // Updates a chess game. It should replace the chess game string corresponding
   // to a given gameID. This is used when players join a game or when a move is
   // made.
-  public void updateGame(int gameID, GameData game) throws DataAccessException {
-    var index = getGameIndexFromID(gameID);
-    if (index < 0 || index > games.size() - 1) {
-      throw new DataAccessException("game not found");
+  public GameData updateGame(int gameID, GameData game) throws ResponseException {
+    var statement =
+        "UPDATE game SET whiteUsername = ?, blackUsername = ?, gameName = ?, json = ? WHERE gameID"
+            + " = ?";
+    var id =
+        executeUpdate(
+            statement,
+            game.getWhiteUsername(),
+            game.getBlackUsername(),
+            game.getGameName(),
+            new Gson().toJson(game.getGame()),
+            gameID);
+    return new GameData(
+        id, game.getWhiteUsername(), game.getBlackUsername(), game.getGameName(), game.getGame());
+  }
+
+  // Retrieve a specified game with the given game ID.
+  public GameData getGame(int gameID) throws ResponseException {
+    try (var conn = DatabaseManager.getConnection()) {
+      var statement = "SELECT * FROM game WHERE gameID = ?";
+      try (var ps = conn.prepareStatement(statement)) {
+        ps.setInt(1, gameID);
+        try (var rs = ps.executeQuery()) {
+          if (rs.next()) {
+            return readGame(rs);
+          }
+        }
+      }
+    } catch (Exception e) {
+      throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
     }
-    games.set(index, game);
+    return null;
+  }
+
+  // Retrieve all games.
+  public Collection<GameData> listGames() throws ResponseException {
+    var result = new ArrayList<GameData>();
+    try (var conn = DatabaseManager.getConnection()) {
+      var statement = "SELECT * FROM game";
+      try (var ps = conn.prepareStatement(statement)) {
+        try (var rs = ps.executeQuery()) {
+          while (rs.next()) {
+            result.add(readGame(rs));
+          }
+        }
+      }
+    } catch (Exception e) {
+      throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
+    }
+    return result;
+  }
+
+  private GameData readGame(ResultSet rs) throws SQLException {
+    var id = rs.getInt("id");
+    var whiteUsername = rs.getString("whiteUsername");
+    var blackUsername = rs.getString("blackUsername");
+    var gameName = rs.getString("gameName");
+    var json = rs.getString("json");
+    var game = new Gson().fromJson(json, ChessGame.class);
+    return new GameData(id, whiteUsername, blackUsername, gameName, game);
   }
 }

server/src/main/java/server/Server.java
@@ -9,12 +9,21 @@ import spark.*;
 
 public class Server {
   private Gson gson = new Gson();
-  private UserDataAccess userDataAccess = new MemoryUserDataAccess();
-  private AuthDataAccess authDataAccess = new MemoryAuthDataAccess();
-  private GameDataAccess gameDataAccess = new MemoryGameDataAccess();
-  private UserService userService = new UserService(userDataAccess, authDataAccess);
-  private GameService gameService = new GameService(authDataAccess, gameDataAccess);
-  private DataService dataService = new DataService(userDataAccess, authDataAccess, gameDataAccess);
+  private UserDataAccess userDataAccess;
+  private AuthDataAccess authDataAccess;
+  private GameDataAccess gameDataAccess;
+  private UserService userService;
+  private GameService gameService;
+  private DataService dataService;
+
+  public Server() throws ResponseException {
+    userDataAccess = new SQLUserDataAccess();
+    authDataAccess = new SQLAuthDataAccess();
+    gameDataAccess = new SQLGameDataAccess();
+    userService = new UserService(userDataAccess, authDataAccess);
+    gameService = new GameService(authDataAccess, gameDataAccess);
+    dataService = new DataService(userDataAccess, authDataAccess, gameDataAccess);
+  }
 
   public int run(int desiredPort) {
     Spark.port(desiredPort);
@@ -32,6 +41,9 @@ public class Server {
           try {
             dataService.clearData();
             return "";
+          } catch (ResponseException e) {
+            response.status(e.getStatusCode());
+            return gson.toJson(new ErrorResponse(e.getMessage()));
           } catch (Exception e) {
             response.status(500);
             return gson.toJson(new ErrorResponse(e.getMessage()));

server/src/main/java/service/GameService.java
@@ -62,11 +62,7 @@ public class GameService {
     } else {
       throw new ResponseException(400, "invalid color");
     }
-    try {
-      gameDataAccess.updateGame(game.getGameId(), game);
-    } catch (DataAccessException e) {
-      throw new ResponseException(500, e.getMessage());
-    }
+    gameDataAccess.updateGame(game.getGameId(), game);
     return game;
   }
 }

server/src/main/java/dataAccess/AuthDataAccess.java
@@ -1,14 +1,15 @@
 package dataAccess;
 
+import exception.ResponseException;
 import model.AuthData;
 
 public interface AuthDataAccess extends DataAccess {
   // Create a new authorization.
-  public void createAuth(AuthData auth);
+  public AuthData createAuth(AuthData auth) throws ResponseException;
 
   // Retrieve an authorization given an authToken.
-  public AuthData getAuth(String authToken);
+  public AuthData getAuth(String authToken) throws ResponseException;
 
   // Delete an authorization so that it is no longer valid.
-  public void deleteAuth(String authToken);
+  public void deleteAuth(String authToken) throws ResponseException;
 }

server/src/main/java/dataAccess/DataAccess.java
@@ -1,6 +1,8 @@
 package dataAccess;
 
+import exception.ResponseException;
+
 interface DataAccess {
   // A method for clearing all data from the database. Used during testing.
-  public void clear();
+  public void clear() throws ResponseException;
 }

server/src/main/java/dataAccess/GameDataAccess.java
@@ -1,19 +1,21 @@
 package dataAccess;
 
+import exception.ResponseException;
 import model.GameData;
+import java.util.Collection;
 
 public interface GameDataAccess extends DataAccess {
   // Create a new game.
-  public void createGame(GameData game);
+  public GameData createGame(GameData game) throws ResponseException;
 
   // Retrieve a specified game with the given game ID.
-  public GameData getGame(int gameID);
+  public GameData getGame(int gameID) throws ResponseException;
 
   // Retrieve all games.
-  public GameData[] listGames();
+  public Collection<GameData> listGames() throws ResponseException;
 
   // Updates a chess game. It should replace the chess game string corresponding
   // to a given gameID. This is used when players join a game or when a move is
   // made.
-  public void updateGame(int gameID, GameData game) throws DataAccessException;
+  public GameData updateGame(int gameID, GameData game) throws ResponseException;
 }

server/src/main/java/dataAccess/MemoryAuthDataAccess.java
@@ -13,8 +13,9 @@ public class MemoryAuthDataAccess implements AuthDataAccess {
   }
 
   // Create a new authorization.
-  public void createAuth(AuthData auth) {
+  public AuthData createAuth(AuthData auth) {
     auths.put(auth.getAuthToken(), auth);
+    return auth;
   }
 
   // Retrieve an authorization given an authToken.

commit 4cb70ac83023898d80a2d643332e99473aae8860

fix(server): allow `NULL` game username columns
These are `NULL` on creation and are only set when a user joins. This
patch also includes some additional error logging to `Server()` to make
debugging easier (especially in production environments).

Fixes: a687147ccf5b ("feat(server/dataAccess): implement SQL DAOs")

Makefile
@@ -13,7 +13,7 @@ format:
 	find . -name "*.java" -type f -print | xargs google-java-format --replace
 
 test:
-	mvn test -Dtest="StandardAPITests#loginInvalidUser" -Dsurefire.failIfNoSpecifiedTests=false
+	mvn test -Dtest="StandardAPITests#goodJoin" -Dsurefire.failIfNoSpecifiedTests=false
 
 db:
 	docker run --name chess -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=chess -d mysql

server/src/main/java/dataAccess/SQLDataAccess.java
@@ -40,7 +40,10 @@ abstract class SQLDataAccess {
           var param = params[i];
           if (param instanceof String p) ps.setString(i + 1, p);
           else if (param instanceof Integer p) ps.setInt(i + 1, p);
-          else if (param == null) ps.setNull(i + 1, NULL);
+          else if (param == null) {
+            System.out.println("Setting param (" + (i + 1) + ") to NULL...");
+            ps.setNull(i + 1, NULL);
+          }
         }
         System.out.println("Executing: " + ps.toString());
         ps.executeUpdate();

server/src/main/java/dataAccess/SQLGameDataAccess.java
@@ -13,10 +13,10 @@ public class SQLGameDataAccess extends SQLDataAccess implements GameDataAccess {
     """
     CREATE TABLE IF NOT EXISTS game (
       gameID int NOT NULL AUTO_INCREMENT,
-      whiteUsername varchar(256) NOT NULL,
-      blackUsername varchar(256) NOT NULL,
+      whiteUsername varchar(256) NULL,
+      blackUsername varchar(256) NULL,
       gameName varchar(256) NOT NULL,
-      json TEXT DEFAULT NULL,
+      json TEXT NULL,
       PRIMARY KEY (gameID),
       FOREIGN KEY (whiteUsername) REFERENCES user(username),
       FOREIGN KEY (blackUsername) REFERENCES user(username)
@@ -38,12 +38,15 @@ public class SQLGameDataAccess extends SQLDataAccess implements GameDataAccess {
   public GameData createGame(GameData game) throws ResponseException {
     var statement =
         "INSERT INTO game (whiteUsername, blackUsername, gameName, json) VALUES (?, ?, ?, ?)";
-    var json = new Gson().toJson(game.getGame());
-    var id =
-        executeUpdate(
-            statement, game.getWhiteUsername(), game.getBlackUsername(), game.getGameName(), json);
-    return new GameData(
-        id, game.getWhiteUsername(), game.getBlackUsername(), game.getGameName(), game.getGame());
+    var json = game.getGame() == null ? null : new Gson().toJson(game.getGame());
+    var whiteUsername = game.getWhiteUsername();
+    var blackUsername = game.getBlackUsername();
+    var gameName = game.getGameName();
+    System.out.println(
+        "Creating game: " + whiteUsername + ", " + blackUsername + ", " + gameName + ", " + json);
+    var id = executeUpdate(statement, whiteUsername, blackUsername, gameName, json);
+    System.out.println("Created game with ID: " + id);
+    return new GameData(id, whiteUsername, blackUsername, gameName, game.getGame());
   }
 
   // Updates a chess game. It should replace the chess game string corresponding
@@ -102,12 +105,12 @@ public class SQLGameDataAccess extends SQLDataAccess implements GameDataAccess {
   }
 
   private GameData readGame(ResultSet rs) throws SQLException {
-    var id = rs.getInt("id");
+    var gameID = rs.getInt("gameID");
     var whiteUsername = rs.getString("whiteUsername");
     var blackUsername = rs.getString("blackUsername");
     var gameName = rs.getString("gameName");
     var json = rs.getString("json");
     var game = new Gson().fromJson(json, ChessGame.class);
-    return new GameData(id, whiteUsername, blackUsername, gameName, game);
+    return new GameData(gameID, whiteUsername, blackUsername, gameName, game);
   }
 }

server/src/main/java/server/Server.java
@@ -25,7 +25,7 @@ public class Server {
       gameService = new GameService(authDataAccess, gameDataAccess);
       dataService = new DataService(userDataAccess, authDataAccess, gameDataAccess);
     } catch (Throwable ex) {
-      System.out.println("Failed to initialize server: " + ex.getMessage());
+      System.err.println("Failed to initialize server: " + ex.getMessage());
       ex.printStackTrace();
     }
   }
@@ -47,6 +47,7 @@ public class Server {
             dataService.clearData();
             return "";
           } catch (ResponseException e) {
+            System.err.println("Error clearing data: " + e.getMessage());
             response.status(e.getStatusCode());
             return gson.toJson(new ErrorResponse(e.getMessage()));
           } catch (Exception e) {
@@ -63,6 +64,7 @@ public class Server {
             UserData user = gson.fromJson(request.body(), UserData.class);
             return gson.toJson(userService.registerUser(user));
           } catch (ResponseException e) {
+            System.err.println("Error registering user: " + e.getMessage());
             response.status(e.getStatusCode());
             return gson.toJson(new ErrorResponse(e.getMessage()));
           } catch (Exception e) {
@@ -79,6 +81,7 @@ public class Server {
             LoginRequest loginRequest = gson.fromJson(request.body(), LoginRequest.class);
             return gson.toJson(userService.loginUser(loginRequest));
           } catch (ResponseException e) {
+            System.err.println("Error logging in: " + e.getMessage());
             response.status(e.getStatusCode());
             return gson.toJson(new ErrorResponse(e.getMessage()));
           } catch (Exception e) {
@@ -96,6 +99,7 @@ public class Server {
             userService.logoutUser(authToken);
             return "";
           } catch (ResponseException e) {
+            System.err.println("Error logging out: " + e.getMessage());
             response.status(e.getStatusCode());
             return gson.toJson(new ErrorResponse(e.getMessage()));
           } catch (Exception e) {
@@ -112,6 +116,7 @@ public class Server {
             String authToken = request.headers("Authorization");
             return gson.toJson(gameService.listGames(authToken));
           } catch (ResponseException e) {
+            System.err.println("Error listing games: " + e.getMessage());
             response.status(e.getStatusCode());
             return gson.toJson(new ErrorResponse(e.getMessage()));
           } catch (Exception e) {
@@ -129,6 +134,7 @@ public class Server {
             GameData gameData = gson.fromJson(request.body(), GameData.class);
             return gson.toJson(gameService.createGame(authToken, gameData));
           } catch (ResponseException e) {
+            System.err.println("Error creating game: " + e.getMessage());
             response.status(e.getStatusCode());
             return gson.toJson(new ErrorResponse(e.getMessage()));
           } catch (Exception e) {
@@ -149,6 +155,7 @@ public class Server {
             JoinGameRequest joinGameRequest = gson.fromJson(request.body(), JoinGameRequest.class);
             return gson.toJson(gameService.joinGame(authToken, joinGameRequest));
           } catch (ResponseException e) {
+            System.err.println("Error joining game: " + e.getMessage());
             response.status(e.getStatusCode());
             return gson.toJson(new ErrorResponse(e.getMessage()));
           } catch (Exception e) {

server/src/main/java/service/GameService.java
@@ -26,8 +26,7 @@ public class GameService {
     if (authDataAccess.getAuth(authToken) == null) {
       throw new ResponseException(401, "unauthorized");
     }
-    gameDataAccess.createGame(gameData);
-    return gameData;
+    return gameDataAccess.createGame(gameData);
   }
 
   public GameData joinGame(String authToken, JoinGameRequest joinGameRequest)
@@ -62,7 +61,6 @@ public class GameService {
     } else {
       throw new ResponseException(400, "invalid color");
     }
-    gameDataAccess.updateGame(game.getGameId(), game);
-    return game;
+    return gameDataAccess.updateGame(game.getGameId(), game);
   }
 }

server/src/main/java/service/UserService.java
@@ -27,8 +27,7 @@ public class UserService {
     userDataAccess.createUser(user);
     String authToken = AuthService.generateNewToken();
     AuthData authData = new AuthData(user.getUsername(), authToken);
-    authDataAccess.createAuth(authData);
-    return authData;
+    return authDataAccess.createAuth(authData);
   }
 
   public AuthData loginUser(LoginRequest loginRequest) throws ResponseException {
@@ -39,8 +38,7 @@ public class UserService {
     }
     String authToken = AuthService.generateNewToken();
     AuthData authData = new AuthData(user.getUsername(), authToken);
-    authDataAccess.createAuth(authData);
-    return authData;
+    return authDataAccess.createAuth(authData);
   }
 
   public void logoutUser(String authToken) throws ResponseException {